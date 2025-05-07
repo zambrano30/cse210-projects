a12eedb31317302ead847360503ef2a3b335aa46
Tu Nombre <Zambranosud18@gmail.com>
2025-05-07 16:16:28 -0500

    Add user input for first and last name in Exercise1 Project

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {firstname}, {lastname} {firstname}.");
+
     }
 }

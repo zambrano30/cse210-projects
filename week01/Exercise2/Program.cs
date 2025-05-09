8ab67d3ae92880b53bad4c04f96f15fb7248fc6c
chriszam <czambranovillavicenc@byupathway.edu>
2025-05-09 01:58:52 -0500

    Implement grade input and evaluation in Exercise2 Project

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Insert your grade: ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+        string letter = "";
+        if (grade >=90)
+        {   
+            letter="A";
+        }
+        else if (grade>=80)
+        {
+            letter="B";
+
+        }
+        else if (grade>=70)
+        {
+            letter="C";
+
+        }
+         else if (grade>=60)
+        {
+            letter="D";
+
+        }
+         else if (grade<60)
+        {
+            letter="F";     
+         }
+
+         Console.WriteLine($"Your grade is : {letter}");
+
+         if (grade >=70)
+         {
+            Console.WriteLine("You passed the course.");
+
+         }
+         else{
+            Console.WriteLine("You failed the course, keep trying the next time.");
+         }
     }
 }

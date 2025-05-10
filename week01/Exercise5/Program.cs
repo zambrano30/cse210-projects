b7901b2c554dd6b3f30f4b7bf477824d1f795ca2
chriszam <czambranovillavicenc@byupathway.edu>
2025-05-10 10:21:00 -0500

    Implement user input for name and favorite number, and calculate/display the square of the number in Exercise5 Project

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        displayMessage();
+        string name=promptUserName(); 
+        int number=promptUserNumber();
+        int squre =calculateSquare(number);
+        displaySquare(number,squre,name);
+
+    
+    }
+    static void displayMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string promptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+        
+    }
+    static int promptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string number = Console.ReadLine();
+        int favoriteNumber = int.Parse(number);
+        return favoriteNumber;
+    }
+    static int calculateSquare(int number)
+    {
+        return number*number;
+    }
+    static void displaySquare(int number, int square, string name)
+    {
+        Console.WriteLine($"Brother {name} the square of {number} is {square}.");
     }
 }
